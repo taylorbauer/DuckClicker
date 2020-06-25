badce432cb853dba00dbc8cf2a981687caba7e6e
Taylor Bauer <taylorbauer@ku.edu>
2020-06-25 15:50:52 -0500

    Dynamic UI elements and code for starting the game and building the world.

--- Assets/Controllers/TileController.cs
+++ Assets/Controllers/TileController.cs
@@ -6,35 +6,48 @@ public class TileController : MonoBehaviour
 {
     public Sprite tileSprite;
 
+    public static TileController tileControllerInstance;
+
     // Maybe updated to move the tiles
     float scrollSpeed = 0;
     List<GameObject> tiles;
-    
+    float tile_Y;  // Y coordinate to spawn tiles at
+    float tileSpawnOffset; // Offsets the first spawn to the left (and adjusts the last accordingly)
+
+    public float animationDelay = 1.0f; // Delay on animation for spawning things in
 
     // Start is called before the first frame update
     void Start()
     {
+        tileControllerInstance = this;
         tiles = new List<GameObject>();
-        float tileSpawnOffset = 5f;  // Offsets the first spawn to the left (and adjusts the last accordingly)
+        tileSpawnOffset = 5f;  
+        tile_Y = 0;
 
-        // Spawn new tiles
-        float tileSpawnTracker = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
-        float tileSpawnTerminus = tileSpawnTracker + Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x + tileSpawnOffset;
-        while (tileSpawnTracker <= tileSpawnTerminus) {
-            GameObject newTile = new GameObject();
-            newTile.AddComponent<SpriteRenderer>();
-            newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
-            newTile.transform.position = new Vector3(tileSpawnTracker - tileSpawnOffset / 2, Camera.main.ScreenToWorldPoint(Vector3.zero).y,0);
-            newTile.transform.SetParent(this.transform, true);
-            tiles.Add(newTile);
-            tileSpawnTracker += newTile.GetComponent<SpriteRenderer>().bounds.size.x;
-        }
-        Debug.Log(tiles.Count + " tiles created");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SpawnTiles() {
+        Invoke("_SpawnTiles", animationDelay);
+    }
+    void _SpawnTiles() {
+        float tileSpawnTracker = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+        float tileSpawnTerminus = tileSpawnTracker + Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x + tileSpawnOffset;
+        while (tileSpawnTracker <= tileSpawnTerminus) {
+            GameObject newTile = new GameObject();
+            newTile.AddComponent<SpriteRenderer>();
+            newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+            newTile.transform.position = new Vector3(tileSpawnTracker - tileSpawnOffset / 2, tile_Y, 0);
+            newTile.transform.SetParent(this.transform, true);
+            tiles.Add(newTile);
+            tileSpawnTracker += newTile.GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+        Debug.Log(tiles.Count + " tiles created");
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,30 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    public static PlayerController playerControllerInstance;
     public Sprite duckSprite;
-    public GameObject duck {get; protected set;}
-    // Start is called before the first frame update
+    public GameObject duck { get; protected set; }
+    public GameObject buttonPrefab;
+    public GameObject textPrefab;
+    public Canvas mainCanvas;
+    public Canvas bottomButtons;
+
+    public float duckOffsetY; // Y offset from center of screen.  Duck pivot is bottom center.
+
+    public float animationDelay = 1.0f; // Delay on animation for spawning things in
+
     void Start()
     {
-        duck = new GameObject();
-        duck.AddComponent<SpriteRenderer>();
-        duck.GetComponent<SpriteRenderer>().sprite = duckSprite;
-        duck.transform.SetParent(this.transform, true);
-        duck.transform.position = new Vector3 (0, -0.11f, 0);
-        duck.name = "duck";
-        duck.SetActive(false);
+        playerControllerInstance = this;
+        InitializeDuck();
+        GenerateDuckButton();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    public void spawnDuck() {
-        duck.SetActive(true);
+    void InitializeDuck()
+    {
+        duck = new GameObject();
+        duck.AddComponent<SpriteRenderer>();
+        duck.GetComponent<SpriteRenderer>().sprite = duckSprite;
+        duck.transform.SetParent(this.transform, true);
+        duck.transform.position = new Vector3(0, duckOffsetY, 0);
+        duck.name = "duck";
+        duck.SetActive(false);
     }
+
+    // Spawns duck and tiles, destroys button
+    void StartGame(GameObject buttonToBeDestroyed)
+    {
+        destoryButton(buttonToBeDestroyed);
+        Invoke("SpawnDuck", animationDelay);
+    }
+
+    void SpawnDuck()
+    {
+        Debug.Log("Spawning duck");
+        duck.SetActive(true);
+        TileController.tileControllerInstance.SpawnTiles();
+        Invoke("SpawnBreadText", animationDelay * 2);
+    }
+
+    void destoryButton(GameObject button)
+    {
+        Destroy(button);
+    }
+    void GenerateDuckButton()
+    {
+        var duckButton = Instantiate(buttonPrefab, mainCanvas.transform.position, Quaternion.identity);
+        duckButton.transform.SetParent(mainCanvas.transform);
+        duckButton.AddComponent<UnityEngine.UI.Button>();
+        var my_button = duckButton.GetComponent<UnityEngine.UI.Button>();
+        my_button.onClick.AddListener(() => StartGame(duckButton));
+    }
+
+    void SpawnBreadText() {
+        var text = Instantiate(textPrefab, bottomButtons.transform.position, Quaternion.identity);
+        text.transform.SetParent(bottomButtons.transform);
+        text.GetComponent<Text>().text = "Bread: 0";
+        Invoke("SpawnBPSText", animationDelay / 2f);
+    }
+
+    void SpawnBPSText() {
+        var text = Instantiate(textPrefab, bottomButtons.transform.position, Quaternion.identity);
+        text.transform.SetParent(bottomButtons.transform);
+        text.GetComponent<Text>().text = "Bread/sec: 0 BPS";
+        Invoke("SpawnWalkButton", animationDelay / 2);
+    }
+
+    void SpawnWalkButton() {
+        var walkButton = Instantiate(buttonPrefab, mainCanvas.transform.position, Quaternion.identity);
+        walkButton.transform.SetParent(bottomButtons.transform);
+        walkButton.AddComponent<UnityEngine.UI.Button>();
+        walkButton.GetComponentInChildren<Text>().text = "Walk";
+        var my_button = walkButton.GetComponent<UnityEngine.UI.Button>();
+    }
+
+    // TODO: Make these three UI elements all at once, immediately disable them, and then enable them on
+    // the timer instead of what I'm doing now
 }
