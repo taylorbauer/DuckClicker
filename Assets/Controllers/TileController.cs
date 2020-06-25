54d26dbb92b7935f0146342dbb7eca13a13ae6ce
Taylor Bauer <taylorbauer@ku.edu>
2020-06-24 20:06:16 -0500

    Initial commit.  Spawning tiles and button to spawn duck.

---
+++ Assets/Controllers/TileController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileController : MonoBehaviour
+{
+    public Sprite tileSprite;
+
+    // Maybe updated to move the tiles
+    float scrollSpeed = 0;
+    List<GameObject> tiles;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tiles = new List<GameObject>();
+        float tileSpawnOffset = 5f;
+
+        // Spawn new tiles
+        float tileSpawnTracker = Camera.main.ScreenToWorldPoint(Vector3.zero).x;
+        float tileSpawnTerminus = tileSpawnTracker + Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x + tileSpawnOffset;
+        while (tileSpawnTracker <= tileSpawnTerminus) {
+            GameObject newTile = new GameObject();
+            newTile.AddComponent<SpriteRenderer>();
+            newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+            newTile.transform.position = new Vector3(tileSpawnTracker - tileSpawnOffset / 2, Camera.main.ScreenToWorldPoint(Vector3.zero).y,0);
+            newTile.transform.SetParent(this.transform, true);
+            tiles.Add(newTile);
+            tileSpawnTracker += newTile.GetComponent<SpriteRenderer>().bounds.size.x;
+        }
+        Debug.Log(tiles.Count + " tiles created");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Sprite duckSprite;
+    public GameObject duck {get; protected set;}
+    // Start is called before the first frame update
+    void Start()
+    {
+        duck = new GameObject();
+        duck.AddComponent<SpriteRenderer>();
+        duck.GetComponent<SpriteRenderer>().sprite = duckSprite;
+        duck.transform.SetParent(this.transform, true);
+        duck.transform.position = new Vector3 (0, -0.11f, 0);
+        duck.name = "duck";
+        duck.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void spawnDuck() {
+        duck.SetActive(true);
+    }
+}
